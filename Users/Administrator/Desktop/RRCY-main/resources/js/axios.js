cf687938943c6f4b1f440a970ba84954c1d10478
kelntro <michaelentero123@gmail.com>
2024-08-01 08:39:45 +0800

    updated by michael

--- Users/Administrator/Desktop/RRCY-main/resources/js/axios.js
+++
@@ -1,8 +0,0 @@
-// resources/js/axios.js
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api', // Replace with your actual backend URL
-});
-
-export default axiosInstance;
